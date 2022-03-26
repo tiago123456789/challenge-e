a10e76a8be6b473f5703ede9b298818a7435099f
Tiago rosa da costa <tiagorosadacost@gmail.com>
2022-03-26 02:23:25 -0300

    Add unit test to case success

--- tests/units/services/BilletService.spec.ts
+++ tests/units/services/BilletService.spec.ts
@@ -1,3 +1,4 @@
+import DigitableLineDto from "../../../src/dtos/DigitableLineResultDto"
 import BilletServiceFactory from "../../../src/factories/BilletServiceFactory"
 import BilletService from "../../../src/services/BilletService"
 
@@ -49,6 +50,26 @@ describe("BilletService", () => {
         try {
             const billetService = new BilletServiceFactory().make({})
             billetService.getDataDigitableLine("21290001192110001210904475617406975870000002000")
+        } catch(error: any) {
+            expect(error.message).toBe("Linha digital tem DV inválidos")
+        }
+    })
+
+    it("Should be return codeBar, amount and expirationDate when digitable line valid", 
+    () => {
+        try {
+            const fakeResult = {
+                "codeBar": "21299758700000020000001121100012100447561740",
+                "amount": "20.00",
+                "expirationDate": "2018-07-16"
+            }
+            const billetService = new BilletServiceFactory().make({})
+            const result: DigitableLineDto = billetService.getDataDigitableLine(
+                "21290001192110001210904475617405975870000002000"
+            )
+            expect(result.amount).toBe(fakeResult.amount)
+            expect(result.expirationDate).toBe(fakeResult.expirationDate)
+            expect(result.codeBar).toBe(fakeResult.codeBar)
         } catch(error: any) {
             expect(error.message).toBe("Linha digital tem DV inválidos")
         }
